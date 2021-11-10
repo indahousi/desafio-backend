89e2de760153cda96c19454fe7c316e25f02437a
diegosantosouza <diegosantos.s@hotmail.com>
2021-11-10 12:19:37 -0300

    add swagger api

--- src/config/App.ts
+++ src/config/App.ts
@@ -2,19 +2,20 @@ import express from 'express'
 import { router } from '../routes/reservas'
 import { connectDB } from '../db/connect'
 import cors from 'cors'
-
+import swaggerDocs from './config-swagger.json'
+import swaggerUi from 'swagger-ui-express'
 
 class App {
     public app: express.Application
     
     constructor() {
         this.app = express()
-    
         this.initializeMiddlewares()
         this.initializeControllers()
         this.initializeDatabase()
+        this.initializeDocumentation()
     }
-    
+
     private initializeMiddlewares(): void {
         this.app.use(express.json())
         this.app.use(cors())
@@ -31,5 +32,9 @@ class App {
             console.log(error)
           }
     }
+
+    private initializeDocumentation (): void {
+        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+      }
 }
 export default new App().app
