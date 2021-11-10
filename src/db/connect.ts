e9247d0cc06662e796e9eb78f27c0e1cb17885da
diegosantosouza <diegosantos.s@hotmail.com>
2021-11-10 01:39:23 -0300

    run CI

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
-let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/desafio'
+let MONGODB_URI =  'mongodb://127.0.0.1:27017/desafio'
 
 const connectDB = () => {
     return mongoose
