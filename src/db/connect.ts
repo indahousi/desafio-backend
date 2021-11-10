d240ec6323f8c127e58e95a2d51f8845ea0098f5
diegosantosouza <diegosantos.s@hotmail.com>
2021-11-10 01:50:57 -0300

    run CI

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
-let MONGODB_URI = process.env.PROD_MONGODB || process.env.DEV_MONGODB
+let MONGODB_URI = process.env.PROD_MONGODB 
 
 const connectDB = () => {
     return mongoose
