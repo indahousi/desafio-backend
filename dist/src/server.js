1a762db1b744bec405eb1e5d1f69b9f9701663c9
diegosantosouza <diegosantos.s@hotmail.com>
2021-11-11 13:29:48 -0300

    update docker

--- dist/src/server.js
+++ dist/src/server.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const App_1 = __importDefault(require("./config/App"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const port = process.env.PORT || 3000;
 App_1.default.listen(port, () => {
     console.log(`Server running on port ${port}`);
