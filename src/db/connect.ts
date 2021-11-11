c20e5dfc872d36ccbcf0e7d3f2110c7878ad877d
diegosantosouza <diegosantos.s@hotmail.com>
2021-11-11 00:07:45 -0300

    update CI

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,15 +2,10 @@ import mongoose, { Mongoose } from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const connect = async (): Promise<Mongoose> => {
+let mongoUrl = process.env.MONGODB_URL
+console.log(mongoUrl)
+console.log(process.env.NODE_ENV)
+export const connect = async (): Promise<Mongoose> =>
+  await mongoose.connect(mongoUrl)
 
-  if (process.env.NODE_ENV === 'test') {
-    const mongoUrl = process.env.MONGODB_URL 
-    return await mongoose.connect(mongoUrl)
-  } else {
-    const mongoUrl = process.env.MONGODB_URL_TEST 
-    return await mongoose.connect(mongoUrl)
-  }
-  
-}
 export const close = (): Promise<void> => mongoose.connection.close()
