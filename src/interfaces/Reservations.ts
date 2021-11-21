017dbddd67c5607ceec1b3674b44ba6b5f7efb16
diegosantosouza <diegosantos.s@hotmail.com>
2021-11-21 11:17:11 -0300

    refactor: IReservations

--- src/interfaces/Reservations.ts
+++ src/interfaces/Reservations.ts
@@ -1,12 +1,10 @@
 import { Document } from "mongoose"
+import { IGuest } from "./Guest"
 
 export interface IReservations extends Document {
     apartment_name: string;
     checkin_date: Date;
     checkout_date: Date;
     number_guests: Number;
-    guest_data: {
-        guest_name: [string];
-        guest_email: [string];
-    }
+    guest_data: Array<IGuest>;
 }
